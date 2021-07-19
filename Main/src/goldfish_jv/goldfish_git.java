39e928b188b5e307937ecf238bfb1a6940e0d010
SuvamNayak07 <67088768+SuvamNayak07@users.noreply.github.com>
2021-07-19 09:49:50 -0700

    This is the beginning of the GoldFish project.
    This is the beginning of the project.

---
+++ Main/src/goldfish_jv/goldfish_git.java
@@ -0,0 +1,11 @@
+package goldfish_jv;
+
+public class goldfish_git {
+
+    /** I will create a git version control system in this file. */
+    /** This file will primarily code the Calendar system that we will
+    * have in place. The Calendar system is not a required feature but
+    * this file will be used in abstraction of that idea. **/
+
+
+}
---
+++ Main/src/goldfish_py/main.py
@@ -0,0 +1,8 @@
+
+class Main:
+    # This is the main file. We will use python to store
+    # the database on this machine. I will update the screen's
+    # application using primarily Python and sometimes Java #
+
+    def main(self):
+        return None
